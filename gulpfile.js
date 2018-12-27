4ed4ba3cd2b40b44129b40104371bca6a57ce81e
唐志华 <tangzhihua@corp.netease.com>
2018-12-27 15:38:22 +0800

    mac添加build+打包命令

--- gulpfile.js
+++ gulpfile.js
@@ -11,4 +11,9 @@ gulp.task('package', ['build'], function() {
     execSync('electron-packager ./ qiyu-quick-start --icon=./bitbug-small.ico --platform=win32 --arch=x64 --out=./outApp --overwrite --electron-version=2.0.8 --ignore="node_modules/*"');
 })
 
+gulp.task('packagemac', ['build'], function() {
+    del(['outAppMac']);
+    execSync('electron-packager ./ qiyu-quick-start --icon=./bitbug-small.icns --platform=darwin --arch=x64 --out=./outAppMac --overwrite --electron-version=2.0.8');
+})
+
 gulp.task('default', ['build', 'package']);
